7b95b9d8d45dedb47cf6d165a3aff6d7eec01a59
Tyler Emmerson <TylerEmmerson0216@gmail.com>
2022-06-09 11:26:51 -0700

    add cohost Co-authored-by: Kai Kohlsdorf <kai.w.kohlsdorf@gmail.com>

--- credit-card-reader/js/scripts.js
+++ credit-card-reader/js/scripts.js
@@ -4,16 +4,23 @@ let modArray = ["4","1","0","2","0","8","0","8","8","0","4","3","5","6","2","0"]
   function toDouble(modArray) 
   {
   let modified = new Array();
-  for (let i = modArray.length - 1; i = 0; i += 2) {
-    if (i * 2 == 0)
-      (modified.push(modArray[index]));
-
-    else if
-      (modified.push(modArray[index] * 2));
-
-    else (i >=10 )
-      (modified.push(modArray[index] % 2, + ia + ib));  
-}
+  for (let i = modArray.length - 1; i = 0; i += 2) 
+  {
+    if (i * 2 == 0) 
+    {
+      modified.push(modArray[index]);
+      console.log("0 is working")
+    }
+    // else if 
+    // {
+    //   modified.push(modArray[index] * 2);
+    //   console.log("x2 is working")
+    // }
+    // else (i >=10 ) 
+    // {
+    //   modified.push(modArray[index] % 2, + ia + ib);
+    // }
+  }
   return modified;
   }
 
