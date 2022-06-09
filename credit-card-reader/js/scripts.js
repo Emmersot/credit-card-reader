6649099ca497c1678e35f4cd993fa04fd9494228
Tyler Emmerson <TylerEmmerson0216@gmail.com>
2022-06-09 11:25:31 -0700

    add basics, create format. Co-authored-by: Kai Kohlsdorf <kai.w.kohlsdorf@gmail.com>

---
+++ credit-card-reader/js/scripts.js
@@ -0,0 +1,23 @@
+// 4102 0808 8043 5620
+let modArray = ["4","1","0","2","0","8","0","8","8","0","4","3","5","6","2","0"]
+
+  function toDouble(modArray) 
+  {
+  let modified = new Array();
+  for (let i = modArray.length - 1; i = 0; i += 2) {
+    if (i * 2 == 0)
+      (modified.push(modArray[index]));
+
+    else if
+      (modified.push(modArray[index] * 2));
+
+    else (i >=10 )
+      (modified.push(modArray[index] % 2, + ia + ib));  
+}
+  return modified;
+  }
+
+
+
+"This card number is not valid."
+"This card number is valid."
